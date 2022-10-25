e3d769b97dce120545ad6659a61dcc2d2af39e65
hanthuthuhtet <htth5499@gmail.com>
2022-10-25 18:19:23 +0630

    text editor

--- l20texteditor/app.js
+++ l20texteditor/app.js
@@ -2,56 +2,94 @@ console.log("h");
 
 var gettextarea = document.getElementById("textarea");
 
+var getdivarea = document.getElementById("divarea");
 
-function boldfun(){
-	gettextarea.style.fontWeight = "bold";
-}
+getdivarea.contentEditable = true ;
+// getdivarea.spellCheck = false;
 
-function italicfun(){
-	gettextarea.style.fontStyle = "italic";
-}
 
-function underllinefun(){
-	gettextarea.style.textDecoration = "underline";
-}
+var getbtns = document.querySelectorAll('.btn'); //NodeList > forEach
 
-function lalgfun(){
+//classList => HTMLcollection => forloop
 
-	gettextarea.style.textAlign = "left";
-}
-function calgfun(){
-	gettextarea.style.textAlign = "center";
-}
+getbtns.forEach(function(getbtn){
 
-function ralgfun(){
+	getbtn.addEventListener("click",function(){
+
+		// var getcommand = getbtn.getAttribute('data-command');
+
+		var getcommand = getbtn.dataset['command'];
+
+		console.log(getcommand);
+
+		document.execCommand(getcommand,false,null);
+
+
+
+	});
+
+});
+
+
+// function boldfun(){
+// 	gettextarea.style.fontWeight = "bold";
+// }
+
+// function italicfun(){
+// 	gettextarea.style.fontStyle = "italic";
+// }
+
+// function underllinefun(){
+// 	gettextarea.style.textDecoration = "underline";
+// }
+
+// function lalgfun(){
+
+// 	gettextarea.style.textAlign = "left";
+// }
+// function calgfun(){
+// 	gettextarea.style.textAlign = "center";
+// }
+
+// function ralgfun(){
 	
-	gettextarea.style.textAlign = "right";
-}
+// 	gettextarea.style.textAlign = "right";
+// }
 
 function upcasefun(){
 	
-	gettextarea.style.textTransform = "uppercase";
+	getdivarea.style.textTransform = "uppercase";
 }
 
 
 function lwcasefun(){
 	
-	gettextarea.style.textTransform = "lowercase";
+	getdivarea.style.textTransform = "lowercase";
 }
 
 
 function capcasefun(){
-	gettextarea.style.textTransform = "capitalize";
+	getdivarea.style.textTransform = "capitalize";
 }
 
 
-function clearfun(){
+// function clearfun(){
 
-	gettextarea.style.fontWeight = "normal";
-	gettextarea.style.fontStyle = "normal";
-	gettextarea.style.textDecoration = "none";
-	gettextarea.style.textAlign = "left";
-	gettextarea.style.textTransform = "none";
-	gettextarea.value = "";
-}
+// 	gettextarea.style.fontWeight = "normal";
+// 	gettextarea.style.fontStyle = "normal";
+// 	gettextarea.style.textDecoration = "none";
+// 	gettextarea.style.textAlign = "left";
+// 	gettextarea.style.textTransform = "none";
+// 	gettextarea.value = "";
+// }
 
+
+
+
+
+// execCommand(aCommandName,aShowDefaultUI,aValueArgument);
+
+
+// aShowDefaultUI
+// true
+// false
